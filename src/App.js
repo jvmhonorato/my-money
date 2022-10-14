174b3907e97a9ae136923b177cd49a8b20a2695b
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-10-14 09:20:36 -0300

    Create Table

--- src/App.js
+++ src/App.js
@@ -14,27 +14,28 @@ const { useGet, usePost, useDelete } = Rest(baseURL)
 
 
 function App() {
-  const data = useGet('movimentacoes/2022-10')
+  // const data = useGet('movimentacoes/2022-10')
+  const data = useGet('meses')
 
   // return [data,post] from usePost turn to [postData, post]
- const [postData, post] = usePost('movimentacoes/2022-10')
+ //const [postData, post] = usePost('movimentacoes/2022-10')
 
- const [deleteData, remove] = useDelete()
+ //const [deleteData, remove] = useDelete()
 
 
 
   const newSave = () => {
-    post({valor: 44, descricao:'Victor'})
+    // post({valor: 44, descricao:'Victor'})
   }
 
   const doRemove = () => {
-    remove('movimentacoes/2022-10/-NEG65cszRfrWj1V1jMw')
+    // remove('movimentacoes/2022-10/-NEG65cszRfrWj1V1jMw')
   }
   
   
-  // const saveNew = () => {
-  //   post({valor: 14, descricao: 'olá'})
-  // }
+  const saveNew = () => {
+    // post({valor: 14, descricao: 'olá'})
+  }
 
   if(data.loading){
     return <h2>Loading...</h2>
@@ -57,13 +58,45 @@ function App() {
         <option>01</option>
         <option>02</option>
       </select>
-      <br/>
+     
+      <button>Adicionar mês</button><br/>
+
+      <table className="table container">
+        <thead>
+          <tr>
+            <th>Mês</th>
+            <th>Previsão de entrada</th>
+            <th>Entrada</th>
+            <th>Previsão de saída</th>
+            <th>Saída</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          Object
+          .keys(data.data)
+          .map(mes => {
+            return (
+              <tr key={mes}>
+              <td>{mes}</td>
+              <td>{data.data[mes].input_forecast}</td>
+              <td>{data.data[mes].input}</td>
+              <td>{data.data[mes].output_forecast}</td>
+              <td>{data.data[mes].output}</td>
+            </tr>
+
+            )
+          })
+          }
+         
+        </tbody>
+      </table>
     
      {JSON.stringify(data)}
-     <button onClick={newSave}>Salvar</button>
+     {/* <button onClick={newSave}>Salvar</button>
      <pre>{JSON.stringify(postData)}</pre>
      <button onClick={doRemove}>Remove</button>
-     <pre>{JSON.stringify(deleteData)}</pre>
+     <pre>{JSON.stringify(deleteData)}</pre> */}
     </div>
   );
 }
